7a9b13c4683818b8950618bc3c1f202a0468693f
TurtysProductions <43997792+TurtysProductions@users.noreply.github.com>
2020-10-04 22:13:21 +0100

    First Commit

---
+++ Assets/Scripts/MovementController2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController2 : MonoBehaviour
+{
+    public float xMovementSpeed = 0.25f;
+    public float yMovementSpeed = 0.2f;
+    public int lives = 1;
+    private Rigidbody2D body2D;
+
+    void Start()
+    {
+        body2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            body2D.velocity = new Vector3(-10f, 0.0f, 0.0f) * xMovementSpeed;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            body2D.velocity = new Vector3(10f, 0.0f, 0.0f) * xMovementSpeed;
+        }
+
+        if (Input.GetKey("w"))
+        {
+            body2D.velocity = new Vector3(0.0f, 10f, 0.0f) * yMovementSpeed;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            body2D.velocity = new Vector3(0.0f, -10f, 0.0f) * yMovementSpeed;
+        }
+    }
+}
